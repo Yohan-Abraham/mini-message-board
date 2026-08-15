53c4faac79479fbe44301066bb90e3f300b4c857
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-08-15 11:59:05 -0700

    style: style form and indexTemplate

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require('node:path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // index route
 app.use('/', indexRoute);
