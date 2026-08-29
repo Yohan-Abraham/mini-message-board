2cf484ad07a832d2491fb9fc4e419343a9d28c4c
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-08-29 14:29:46 -0700

    add isLocal variable

--- models/pool.js
+++ models/pool.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isLocal = process.env.DATABASE_URL?.includes('localhost');
+
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: isLocal ? false : { rejectUnauthorized: false },
